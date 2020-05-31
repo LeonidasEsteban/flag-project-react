549d2bf2fb7435822502ba8c17d7655a6a4eddb7
Leonidas Esteban <leonidasesteban@gmail.com>
2020-05-30 20:15:53 -0500

    boilerplate done with redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CountryList from './country-list'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const initialState = {
+  countryList: []
+}
+
+function reducer(state, action) {
+  console.log(action)
+  switch (action.type) {
+    case 'SET_COUNTRY_LIST': {
+      console.log('voy a actualizar la lista de paises')
+      return { ...state, countryList: action.payload }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+const store = createStore(reducer, initialState)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CountryList />
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/country.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CountryStyled = styled.div`
+  width: 264px;
+  text-align: left;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 0 0 7px 2px rgba(0,0,0,.03);
+  img {
+    width: 100%;
+    height: 160px;
+    object-fit: cover;
+  }
+  .details {
+    padding: 1.5em;
+  }
+  h2 {
+    margin: 0;
+    margin-bottom: 1rem;
+    font-size: 18px;
+    font-weight: 700;
+  }
+  p {
+    font-size: .9em;
+    margin-bottom: .5rem;
+  }
+`
+
+function Country({
+  flag,
+  name,
+  population,
+  region,
+  capital,
+}) {
+  return (
+    <CountryStyled>
+      <img loading="lazy" src={flag} alt="" />
+      <div className="details">
+        <h2>{name}</h2>
+        <p><strong>Population:</strong> {population}</p>
+        <p><strong>Region:</strong> {region}</p>
+        <p><strong>Capital:</strong> {capital}</p>
+      </div>
+    </CountryStyled>
+  )
+}
+
+export default Country
