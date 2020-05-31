3856ccbd1b653c49208adaba50e9bbce6e758188
Oscar Alfredo  Cruz Rovira <cruzrovira@gmail.com>
2020-05-30 19:39:05 -0600

    GET_COUNTRY_LIST_REGION

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import './App.css';
-import CountryList from './country-list'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import React from "react";
+import "./App.css";
+import CountryList from "./country-list";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 const initialState = {
-  countryList: []
-}
+  countryList: [],
+  countryListRegion: [],
+};
 
 function reducer(state, action) {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
-    case 'SET_COUNTRY_LIST': {
-      console.log('voy a actualizar la lista de paises')
-      return { ...state, countryList: action.payload }
+    case "SET_COUNTRY_LIST": {
+      console.log("voy a actualizar la lista de paises");
+      return { ...state, countryList: action.payload };
+    }
+    case "GET_COUNTRY_LIST_REGION": {
+      let newCountryList = state.countryList;
+      let countryListRegion = newCountryList.filter(
+        (item) => item.region === action.payload
+      );
+      return { ...state, countryListRegion: countryListRegion };
     }
     default: {
-      return state
+      return state;
     }
   }
 }
 
-const store = createStore(reducer, initialState)
+const store = createStore(reducer, initialState);
 
 function App() {
   return (
