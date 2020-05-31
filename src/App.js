a02b65d7f7d8a1e8d9a74f79e60362ca6573dac4
Manuel Galindez <manuelgalindez81@gmail.com>
2020-05-30 21:42:06 -0500

    Remove redux

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React from 'react';
 import './App.css';
 import CountryList from './country-list'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 
-const initialState = {
-  countryList: []
-}
-
-function reducer(state, action) {
-  console.log(action)
-  switch (action.type) {
-    case 'SET_COUNTRY_LIST': {
-      console.log('voy a actualizar la lista de paises')
-      return { ...state, countryList: action.payload }
-    }
-    default: {
-      return state
-    }
-  }
-}
-
-const store = createStore(reducer, initialState)
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <CountryList />
-      </div>
-    </Provider>
+    <div className="App">
+      <CountryList />
+    </div>
   );
 }
 
