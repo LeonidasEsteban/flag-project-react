51824e723e15ca94fc59b819749a4b1f72edf154
leotalorac <leotalorac@unal.edu.co>
2020-05-30 20:53:02 -0500

    reto 1

--- src/country-list.js
+++ src/country-list.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useReducer } from 'react'
 import styled from 'styled-components'
 import Country from './country'
 import { useSelector, useDispatch } from 'react-redux'
@@ -12,10 +12,20 @@ const CountryListStyled = styled.div`
   border: 1px solid red;
   padding: 4em 2em;
 `
-
+const CountryListStyledDark = styled.div`
+  display: grid;
+  grid-row-gap: 2.3em;
+  /* grid-template-columns: 1fr 1fr 1fr; */
+  background: var(--backgound-dark);
+  justify-content: center;
+  border: 1px solid red;
+  padding: 4em 2em;
+  color:var(--white)
+`
 function CountryList() {
   const dispatch = useDispatch()
   const countryList = useSelector((state) => state.countryList)
+  const countryListFiltered = useSelector((state) => state.countryListByName)
   console.log('el estado total de mi app es', countryList)
   // const [countryList, setCountryList] = useState([])
   useEffect(() => {
@@ -35,10 +45,18 @@ function CountryList() {
         console.log('hubo un error, que dolor que dolo que pena')
       })
   }, [dispatch])
+  let handlerFilter = (e) =>{
+    dispatch({
+      type:'GET_COUNTRY',
+      payload:e.target.value
+    })
+    console.log(countryListFiltered)
+  }
   return (
-    <CountryListStyled>
+    <CountryListStyledDark>
+      <input type="text" onChange={handlerFilter}></input>
       {
-        countryList.map(({ name, flag, population, capital, region, }) => {
+        (countryListFiltered) && countryListFiltered.map(({ name, flag, population, capital, region, }) => {
           return (
             <Country
               flag={flag}
@@ -51,7 +69,7 @@ function CountryList() {
           )
         })
       }
-    </CountryListStyled>
+    </CountryListStyledDark>
   )
 }
 
