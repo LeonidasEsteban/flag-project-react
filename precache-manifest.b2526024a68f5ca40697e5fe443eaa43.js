a05c067a091975266bc4ec6e90a0b2b0b01173c9
Leonidas Esteban <leonidasesteban@gmail.com>
2020-05-31 01:33:35 -0500

    Updates

---
+++ precache-manifest.b2526024a68f5ca40697e5fe443eaa43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6187810d698e900e37e523b6cf222b05",
+    "url": "/flag-project-react/index.html"
+  },
+  {
+    "revision": "d898ac36cf35a5317d2e",
+    "url": "/flag-project-react/static/css/main.c2c71432.chunk.css"
+  },
+  {
+    "revision": "f5a517d5acf281f8b5d2",
+    "url": "/flag-project-react/static/js/2.13828fee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flag-project-react/static/js/2.13828fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d898ac36cf35a5317d2e",
+    "url": "/flag-project-react/static/js/main.0f27481a.chunk.js"
+  },
+  {
+    "revision": "bfdda261314c0e6d07d3",
+    "url": "/flag-project-react/static/js/runtime-main.b644b093.js"
+  }
+]);
