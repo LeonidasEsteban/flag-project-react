52b7ae0b100b1e62239d2927e14d4b604cd1701b
Ruben A Garcia Acosta <ruben@MacBook-Pro-de-Ruben.local>
2020-06-04 19:47:31 -0600

    [ADD] funcionalidad en select implesionante

--- src/Region.js
+++ src/Region.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components'
-
-const RegionFilterStyled = styled.select`
-  padding: 1.3em;
-  border: none;
+const RegionFilterStyled = styled.div`
+@import url('https://fonts.google.com/specimen/Nunito+Sans');
+* {
+    box-sizing: border-box;
+}
+.dropdown {
   border-radius: 5px;
-  outline: 0;
-  box-shadow: 0 2px 9px 0 rgba(0,0,0,.05);
+  background-color: var(--white);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.06), 0 1px 2px rgba(0, 0, 0, 0.12);
+  cursor: pointer;
+  text-align: center;
+  font-size: 12px;
+  padding: 20px;
+  position: relative;
   background: var(--white);
-  color: var(--black);
+}
+.btn-toggle .fas {
+  display: none;
+}
+
+body.dark .btn-toggle .fas {
+  display: inline-block;
+}
+
+
+.dropdown .fa-chevron-down {
+  margin-left: 10px;
+}
+
+.dropdown ul {
+border-radius: 4px;
+  background-color: var(--background-el);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.06), 0 1px 2px rgba(0, 0, 0, 0.12);
+  display: none;
+  padding: 10px;
+  text-align: center;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  list-style-type: none;
+  background: var(--white);
+  width: 100%;
+  z-index: 99;
+}
+
+.dropdown.open ul {
+  display: block;
+}
+
+.dropdown ul li {
+  margin: 10px 0;
+}
+
 `
 
 const filterByRegionAction = (regionSelected) => {
@@ -21,27 +65,30 @@ const filterByRegionAction = (regionSelected) => {
 
 export const Region = () => {
   const dispatch = useDispatch();
+  const [dropdown, setdropdown] = useState(false)
   const filterByRegion = useSelector((state) => state.filterByRegion);
 
-  /**
-   * Disptach filterByRegion action
-   *
-   * @param {React.SyntheticEvent} selectEvent
-   */
   const onRegionChange = (selectEvent) => {
-    const value = selectEvent.target.value;
 
-    dispatch(filterByRegionAction(value));
+    dispatch(filterByRegionAction(selectEvent));
   }
 
   return (
-    <RegionFilterStyled onChange={onRegionChange} value={filterByRegion}>
-      <option value="">Filter by region</option>
-      <option value="Africa">Africa</option>
-      <option value="Americas">Americas</option>
-      <option value="Asia">Asia</option>
-      <option value="Europe">Europe</option>
-      <option value="Oceania">Oceania</option>
+    <>
+    <RegionFilterStyled>
+
+    <div className={`${dropdown?'dropdown open':'dropdown'}`} onClick={()=> setdropdown(!dropdown)} id="filter">
+          Filter by Region
+          <i className="fas fa-chevron-down"></i>
+          <ul>
+            <li onClick={()=>onRegionChange('Africa')}>Africa</li>
+            <li onClick={()=>onRegionChange('Americas')}>Americas</li>
+            <li onClick={()=>onRegionChange('Asia')}>Asia</li>
+            <li onClick={()=>onRegionChange('Europe')}>Europe</li>
+            <li onClick={()=>onRegionChange('Oceania')}>Oceania</li>
+          </ul>
+        </div>
     </RegionFilterStyled>
+    </>
   )
 }
