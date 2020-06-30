30db7c41594adfc99c04e6465ba1d8c90690bff2
Abel Ponce <vnponce8@gmail.com>
2020-06-30 09:27:51 -0500

    testing input text

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,10 @@
+const firstCountry = '[data-testid="country"]:nth-child(1)';
 describe('Layout', () => {
   it('Assert page render data correctly', () => {
     cy.visit('/');
     cy.get('h1').should('contain', 'Where in the world?');
 
-    cy.get('[data-testid="country"]:nth-child(1)').within($country => {
+    cy.get(firstCountry).within($country => {
       cy.get('img').should('have.attr', 'src', 'https://restcountries.eu/data/afg.svg')
       cy.get('[data-testid="name"]').should('contain', 'Afghanistan');
       cy.get('[data-testid="population"]').should('contain', '27657145');
@@ -14,16 +15,27 @@ describe('Layout', () => {
 })
 
 describe('filters', function () {
-  it.only('should filter by region', function () {
+  it('should filter by region', function () {
     cy.visit('/');
     // having Afghanistan at first position
-    cy.get('[data-testid="country"]:nth-child(1) [data-testid="name"]').should('contain', 'Afghanistan');
+    cy.get(`${firstCountry} [data-testid="name"]`).should('contain', 'Afghanistan');
 
     // When user select Americas region
     cy.get('#filter').click();
     cy.get('ul > :nth-child(2)').click();
 
     // Assert Anguilla is at first position now
-    cy.get('[data-testid="country"]:nth-child(1) [data-testid="name"]').should('contain', 'Anguilla');
+    cy.get(`${firstCountry} [data-testid="name"]`).should('contain', 'Anguilla');
+  });
+  it.only('should filter by text', function () {
+    cy.visit('/');
+    // having Afghanistan at first position
+    cy.get(`${firstCountry} [data-testid="name"]`).should('contain', 'Afghanistan');
+
+    // When user select Americas region
+    cy.get('input').type('Mex');
+
+    // Assert Anguilla is at first position now
+    cy.get(`${firstCountry} [data-testid="name"]`).should('contain', 'Mexico');
   });
 });
